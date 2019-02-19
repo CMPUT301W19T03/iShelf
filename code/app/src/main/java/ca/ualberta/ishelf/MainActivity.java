f03fefe0dcda49d94789ffe77ee15720d28f2c0b
jscottgray <jsgray1@ualberta.ca>
2019-02-19 13:40:40 -0700

    description of main activity

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+/**
+ * MainActivity
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
