bfec3dca5e01358a74faf959308a9a565b0662f9
Abdulrahman Alattas <rmnattas@gmail.com>
2019-02-20 16:40:57 -0700

    Firebase test for get methods

---
+++ code/app/src/main/java/ca/ualberta/ishelf/Firebase.java
@@ -0,0 +1,29 @@
+package ca.ualberta.ishelf;
+
+import java.util.ArrayList;
+
+public class Firebase {
+
+    private final String link = "Firebase_Link";
+
+    public User getUser(String username) {
+
+    }
+
+    public Book getBook(String bookID) {
+
+    }
+
+    public ArrayList<Book> getBooks() {
+
+    }
+
+    public ArrayList<Book> getAvailableBooks() {
+
+    }
+
+    public ArrayList<Book> getUserBooks(String username) {
+
+    }
+
+}
