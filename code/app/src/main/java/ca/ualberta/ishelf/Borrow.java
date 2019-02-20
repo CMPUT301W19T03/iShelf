e8058df423003fb247c554bcc04a401d786abba1
mehrabmehdi <mehrabmehdi@gmail.com>
2019-02-20 16:41:48 -0700

    mehrab-test commit

--- code/app/src/main/java/ca/ualberta/ishelf/Borrow.java
+++ code/app/src/main/java/ca/ualberta/ishelf/Borrow.java
@@ -1,30 +1,32 @@
-//package ca.ualberta.ishelf;
-//
-//public class Borrow {
-//
-//    private User lender;
-//
-//
-//
-//    private User borrower;
-//
-//    public User getLender() {
-//        return lender;
-//    }
-//
-//    public void setLender(User lender) {
-//        this.lender = lender;
-//    }
-//
-//    public User getBorrower() {
-//        return borrower;
-//    }
-//
-//    public void setBorrower(User borrower) {
-//        this.borrower = borrower;
-//    }
-//
-//
-//
-//
-//}
+package ca.ualberta.ishelf;
+
+public class Borrow {
+
+    private User lender;
+
+
+
+    private User borrower;
+
+
+    public User getLender() {
+        return lender;
+    }
+
+
+    public void setLender(User lender) {
+        this.lender = lender;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+
+
+
+}
--- code/app/src/test/java/ca/ualberta/ishelf/BorrowTest.java
+++ code/app/src/test/java/ca/ualberta/ishelf/BorrowTest.java
@@ -1,5 +1,51 @@
 package ca.ualberta.ishelf;
 
-public class BorrowTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class BorrowTest {
+    @Test
+    public void setLendertest(){
+        User user = new User();
+        Borrow borrow = new Borrow();
+        borrow.setLender(user);
+
+
+        Assert.assertEquals(user, borrow.getLender());
+
+    }
+
+    @Test
+    public void getLendertest(){
+        User user = new User();
+        Borrow borrow = new Borrow();
+        borrow.setLender(user);
+
+
+        Assert.assertEquals(user, borrow.getLender());
+
+    }
+
+    @Test
+    public void setBorrowertest(){
+        User user = new User();
+        Borrow borrow = new Borrow();
+        borrow.setBorrower(user);
+
+
+        Assert.assertEquals(user, borrow.getBorrower());
+
+
+    }
+
+    @Test
+    public void getBorrowertest(){
+        User user = new User();
+        Borrow borrow = new Borrow();
+        borrow.setBorrower(user);
+
+
+        Assert.assertEquals(user, borrow.getBorrower());
+
+    }
 }
