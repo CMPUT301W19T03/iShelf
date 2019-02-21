717609eddf9c7fc617dcaa44d525d312a9e593bc
Evan <ytl@ualberta.ca>
2019-02-21 10:09:46 -0700

    Evan - edited

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import R.java;
 
 /**
  * MainActivity
