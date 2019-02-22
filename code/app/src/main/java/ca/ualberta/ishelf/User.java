3047c297a33a647cf4a0ec75d7461fd95397458b
Evan <ytl@ualberta.ca>
2019-02-22 12:07:39 -0700

    Evan - edited

--- code/app/src/main/java/ca/ualberta/ishelf/User.java
+++ code/app/src/main/java/ca/ualberta/ishelf/User.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class User {
     private String username;
-    private ArrayList<Book> borrowerBooks;
-    private ArrayList<Book> ownedBooks;
+    private ArrayList<UUID> borrowedBooks;
+    private ArrayList<UUID> ownedBooks;
     private Rating rating;
     private ArrayList<Request> listofRequests;
     private ArrayList<Rating> lenderRatings;
     private ArrayList<Rating> borrowerRatings;
     private String state; //user needs to know if it's acting as a lender or borrower
+    private String phoneNum;
+    private String email;
 
     public String getUsername() {
         return username;
@@ -28,11 +30,11 @@ public class User {
         this.rating = rating;
     }
 
-    public ArrayList<Book> getOwnedBooks() {
+    public ArrayList<UUID> getOwnedBooks() {
         return ownedBooks;
     }
 
-    public void setOwnedBooks(ArrayList<Book> ownedBooks) {
+    public void setOwnedBooks(ArrayList<UUID> ownedBooks) {
         this.ownedBooks = ownedBooks;
     }
 
@@ -44,12 +46,12 @@ public class User {
         this.state = state;
     }
 
-    public ArrayList<Book> getBorrowerBooks() {
-        return borrowerBooks;
+    public ArrayList<UUID> getBorrowedBooks() {
+        return borrowedBooks;
     }
 
-    public void setBorrowerBooks(ArrayList<Book> borrowerBooks) {
-        this.borrowerBooks = borrowerBooks;
+    public void setBorrowedBooks(ArrayList<UUID> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
     }
 
     public ArrayList<Request> getListofRequests() {
@@ -76,16 +78,48 @@ public class User {
         this.borrowerRatings = borrowerRatings;
     }
 
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (toString().indexOf("@") >= 0) {
+            this.email = email;
+        } // else it should popup something like "fake ass email bro" and prompt for re-enter
+    }
     /*should users be able to add books or delete/edit ratings or is that all firebase*/
     //these methods should be public
-    private void deleteBook(Book book){
-        for (int i = 0; i < ownedBooks.size(); i++){
-            if (ownedBooks.get(i) == book){
-                ownedBooks.remove(i);
+    public void addBook(UUID book){
+        if (state == "borrower"){
+            borrowedBooks.add(book);
+        }else {
+            ownedBooks.add(book);
+        }
+    }
+    public void deleteBook(UUID book){
+        if (state == "borrower"){
+            for (int i = 0; i < ownedBooks.size(); i++){
+                if (ownedBooks.get(i) == book){
+                    ownedBooks.remove(i);
+                }
+            }
+        }else {
+            for (int i = 0; i < ownedBooks.size(); i++){
+                if (ownedBooks.get(i) == book){
+                    ownedBooks.remove(i);
+                }
             }
         }
     }
-    private void addRating(Rating rating){
+    public void addRating(Rating rating){
         if (state == "borrower"){
             borrowerRatings.add(rating);
         }else {
--- code/app/src/test/java/ca/ualberta/ishelf/UserClassTest.java
+++ code/app/src/test/java/ca/ualberta/ishelf/UserClassTest.java
@@ -4,9 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 public class UserClassTest {
     @Test
+    private void addBookTest{
+        User Evan = new User();
+        Evan.setState("owner");
+        UUID fiftyShadesofGrey = new UUID;
+        Evan.addBook(fiftyShadesofGrey);
+        assertTrue(Evan.getOwnedBooks().contains(fiftyShadesofGrey));
+    }
+    @Test
     private void deleteBookTest{
         User Evan = new User();
-        Book fiftyShadesofGrey = new Book;
+        Evan.setState("owner");
+        UUID fiftyShadesofGrey = new UUID;
         Evan.deleteBook(fiftyShadesofGrey);
         assertFalse(Evan.getOwnedBooks().contains(fiftyShadesofGrey));
     }
@@ -18,4 +27,5 @@ public class UserClassTest {
         Evan.addRating(utterGarbage);
         assertTrue(Evan.getBorrowerRatings().contains(utterGarbage));
     }
+
 }
