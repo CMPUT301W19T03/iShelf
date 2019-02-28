bf22cd89fe1ec2960a4cdb71eda994895876ddcd
mehrabmehdi <mehrabmehdi@gmail.com>
2019-02-27 18:56:16 -0700

    made fix to the attribute name

--- code/app/src/main/java/ca/ualberta/ishelf/Book.java
+++ code/app/src/main/java/ca/ualberta/ishelf/Book.java
@@ -2,6 +2,8 @@ package ca.ualberta.ishelf;
 
 
 import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -11,7 +13,7 @@ import java.util.UUID;
  * Signatures created by Jeremy
  *
  */
-public class Book {
+public class Book implements Parcelable {
     private String owner;
     private String name;
     private String description;
@@ -20,7 +22,70 @@ public class Book {
     private ArrayList<Rating> ratings = new ArrayList<Rating>(); // Error due to no Rating class yet
     private UUID id; // changed from int to UUID
     private Image photo;
+    private String year;
+    private String genre;
+    private String author;
 
+    public  Book(){}
+    public Book(String name, String description, Long ISBN, String year, String genre, String author) {
+        this.name = name;
+        this.description = description;
+        this.ISBN = ISBN;
+        this.year = year;
+        this.genre = genre;
+        this.author = author;
+    }
+
+    protected Book(Parcel in) {
+        owner = in.readString();
+        name = in.readString();
+        description = in.readString();
+        if (in.readByte() == 0) {
+            ISBN = null;
+        } else {
+            ISBN = in.readLong();
+        }
+        status = in.readInt();
+        year = in.readString();
+        genre = in.readString();
+        author = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
 
     // Getters and Setters
@@ -112,4 +177,25 @@ public class Book {
         ratings.add(rating);
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(owner);
+        dest.writeString(name);
+        dest.writeString(description);
+        if (ISBN == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeLong(ISBN);
+        }
+        dest.writeInt(status);
+        dest.writeString(year);
+        dest.writeString(genre);
+        dest.writeString(author);
+    }
 }
