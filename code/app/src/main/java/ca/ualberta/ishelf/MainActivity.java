8ad84b93f0ab732391335f5a9b120edc1fe65869
FaisalAbutarab <abutarab@ualberta.ca>
2019-03-02 18:10:51 -0700

    Added fragment to MainActivity

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -3,6 +3,7 @@ package ca.ualberta.ishelf;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -24,18 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    selectedFragment = new BorrowFragment();
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    //selectedFragment = new FavoritesFragment();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    //selectedFragment = new SearchFragment();
+                    break;
             }
-            return false;
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+
+            return true;
         }
     };
 
