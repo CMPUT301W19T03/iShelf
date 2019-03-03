fb508e1794d55ab78093dcdf1014cc5db4ede196
jscottgray <jsgray1@ualberta.ca>
2019-03-03 14:07:07 -0700

    get from database the user from the passed in username

--- code/app/src/main/java/ca/ualberta/ishelf/ViewProfileActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/ViewProfileActivity.java
@@ -2,12 +2,23 @@ package ca.ualberta.ishelf;
 
 import android.content.Context;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.util.Linkify;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * ViewProfileActivity
@@ -23,26 +34,74 @@ public class ViewProfileActivity extends AppCompatActivity {
     TextView tvContactInformation = (TextView) findViewById(R.id.tvContactInformation);
     */
     Button editProfileButton;
+    private final String link = "https://ishelf-bb4e7.firebaseio.com";
+    private Firebase ref;
+    TextView tvUsername;
+    TextView tvPhoneNum;
+    TextView tvEmail;
+    User user;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+        Tag TAG = "editProfile";
 
-        TextView tvUsername = (TextView) findViewById(R.id.tvUsername);
-        TextView tvPhoneNum = (TextView) findViewById(R.id.tvPhoneNum);
-        TextView tvEmail = (TextView) findViewById(R.id.tvEmail);
-        User user;
+        tvUsername = (TextView) findViewById(R.id.tvUsername);
+        tvPhoneNum = (TextView) findViewById(R.id.tvPhoneNum);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
 
-
-        // TODO: put below into a try/catch code block for if a user is not sent in with an activity
-        Bundle bundle = getIntent().getExtras();
-        user = (User) bundle.getSerializable("User");
-
-        editProfileButton = findViewById(R.id.editProfileButton);
         // get the signed-in user's username
         String currentUsername = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE).getString("username", null);
 
+        if (getIntent().hasExtra("Username")) {
+            Bundle bundle = getIntent().getExtras();
+            username = (String) bundle.getSerializable("Username");
+        } else {
+            // When no username is passed along, assume we want to view the signed-in user's info
+            Log.d(TAG, "onCreate: no User info passed along");
+            // no extra, therefore working with our username
+            username = currentUsername;
+        }
+
+        // Get user from the passed in username
+        Firebase.setAndroidContext(this);
+        ref = new Firebase(link);
+
+        // get reference to specific entry
+        Firebase tempRef = ref.child("Users").child(username);
+        final ArrayList<User> userList = new ArrayList<User>();
+        // create a one time use listener to immediately access datasnapshot
+        tempRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String jUser = dataSnapshot.getValue(String.class);
+                Log.d("jUser", jUser);
+                if (jUser != null) {
+                    // Get user object from Gson
+                    Gson gson = new Gson();
+                    Type tokenType = new TypeToken<User>(){}.getType();
+                    User user = gson.fromJson(jUser, tokenType); // here is where we get the user object
+
+                    // fill the fields with their current info
+                    tvUsername.setText(user.getUsername());
+                    tvPhoneNum.setText("PHONE: " + user.getPhoneNum());
+                    tvEmail.setText("EMAIL: " + user.getEmail());
+                    Linkify.addLinks(tvPhoneNum, Linkify.PHONE_NUMBERS); // make phone number callable/textable
+                    Linkify.addLinks(tvEmail, Linkify.EMAIL_ADDRESSES); // make email clickable
+
+                    Log.d("Confirm", user.getUsername());
+                    userList.add(user);
+                } else {
+                    Log.d("FBerror1", "User doesn't exist or string is empty");
+                }
+                Log.d("Size", String.valueOf(userList.size()));
+            }
+            });
+
+
+        editProfileButton = findViewById(R.id.editProfileButton);
         if (user.getUsername() == currentUsername){
             // if profile we are viewing is the logged in user's
             editProfileButton.setVisibility(View.VISIBLE);
@@ -51,25 +110,13 @@ public class ViewProfileActivity extends AppCompatActivity {
         }
 
         /*
-        if (){//(bundle != null){
-            user = (User) bundle.getSerializable("User");
-        } else {
-            user = new User();
-            user.setUsername("Jeremy71");
-            user.setPhoneNum("604-123-9876");
-            Rating rating = new Rating();
-            rating.setRating(4);
-            rating.setComment("Very Fast");
-            user.setRating(rating);
-            user.setEmail("jsgray1@ualberta.ca");
-        }
-        */
-
         tvUsername.setText(user.getUsername());
         tvPhoneNum.setText("PHONE: " + user.getPhoneNum());
         tvEmail.setText("EMAIL: " + user.getEmail());
+
         Linkify.addLinks(tvPhoneNum, Linkify.PHONE_NUMBERS); // make phone number callable/textable
         Linkify.addLinks(tvEmail, Linkify.EMAIL_ADDRESSES); // make email clickable
+        */
     }
 
     public void EditProfile(){
