b63b48edec8e01e4a2d0eaf99f93c1a043de2a5f
Evan <ytl@ualberta.ca>
2019-03-05 20:01:14 -0700

    hello

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
