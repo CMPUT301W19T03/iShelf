913aff75f242f4958c1498d5644c4d332ffab339
rkimpins <rkimpins@ualberta.ca>
2019-03-09 10:13:45 -0700

    Added basic functionality for displaying a list of requests. Still need to ensure firebase is working properly, implement accepting a request, better format the boxes, and adding a notification when a request is accepted

--- code/app/src/main/java/ca/ualberta/ishelf/Request.java
+++ code/app/src/main/java/ca/ualberta/ishelf/Request.java
@@ -3,16 +3,58 @@ package ca.ualberta.ishelf;
 import java.util.Date;
 
 public class Request {
-
+    private String owner;
+    private String borrower;
+    private String bookName;
+    private int borrowerRating;
+    private Date timeRequested;
 
     public Request(Date timeRequested) {
         this.timeRequested = timeRequested;
     }
 
-    private Date timeRequested;
+    public Request(Date timeRequested, String owner, String borrower,
+                   String bookName, int borrowerRating) {
+        this.timeRequested = timeRequested;
+        this.owner = owner;
+        this.borrower = borrower;
+        this.bookName = bookName;
+        this.borrowerRating = borrowerRating;
+    }
+
 
     public Date getTimeRequested() {
         return timeRequested;
     }
 
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    @Override
+    public String toString() {
+        String message = "User " + borrower + " with rating " + borrowerRating +
+                " requested to borrow " + bookName + " at " + timeRequested.toString();
+        return message;
+    }
 }
