bfcd6ebd1829c00518cd72439409cee000002e19
rkimpins <rkimpins@ualberta.ca>
2019-03-09 10:53:10 -0700

    Merge branch 'master' of https://github.com/CMPUT301W19T03/iShelf into listofrequestsrkimpins
    # Conflicts:
    #	code/app/src/main/AndroidManifest.xml

--- code/app/src/main/java/ca/ualberta/ishelf/ViewProfileActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/ViewProfileActivity.java
@@ -88,8 +88,7 @@ public class ViewProfileActivity extends AppCompatActivity {
         tvEmail = (TextView) findViewById(R.id.tvEmail);
         ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         editProfileButton = (Button) findViewById(R.id.editProfileButton);
-        //TODO why is this causing an error?
-        //signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton = (Button) findViewById(R.id.signOutButton);
 
 
         // Retrieve the signed-in user's username
