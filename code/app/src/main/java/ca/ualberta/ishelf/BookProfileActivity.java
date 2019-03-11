3f458d59249b11b7f9d33134ce37a75474f6967e
mehrabmehdi <mehrabmehdi@gmail.com>
2019-03-11 12:01:24 -0600

    Fixed listofRequests

--- code/app/src/main/java/ca/ualberta/ishelf/BookProfileActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/BookProfileActivity.java
@@ -75,7 +75,10 @@ public class BookProfileActivity extends AppCompatActivity {
         galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Bundle extras = new Bundle();
+                extras.putParcelable("sent_book", passedBook);
                 Intent intent = new Intent(view.getContext(), GalleryActivity.class);
+                intent.putExtras(extras);
                 startActivity(intent);
             }
         });
