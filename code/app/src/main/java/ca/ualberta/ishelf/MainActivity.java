076a0f4a3f24b52f0d1470a389090560741cc124
Evan <ytl@ualberta.ca>
2019-03-21 14:48:50 -0600

    Merge branch 'master' of https://github.com/CMPUT301W19T03/iShelf into myBook/Evan
    # Conflicts:
    #	code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -1,34 +1,50 @@
 package ca.ualberta.ishelf;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.app.SearchManager;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.SearchView;
-import android.widget.SearchView.OnQueryTextListener;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.Activity;
+        import android.app.NotificationChannel;
+        import android.app.NotificationManager;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.graphics.Color;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.design.widget.BottomNavigationView;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.NotificationCompat;
+        import android.support.v4.app.NotificationManagerCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.app.SearchManager;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.SearchView;
+        import android.widget.SearchView.OnQueryTextListener;
+        import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+        import com.firebase.client.ChildEventListener;
+        import com.firebase.client.DataSnapshot;
+        import com.firebase.client.Firebase;
+        import com.firebase.client.FirebaseError;
+        import com.firebase.client.ValueEventListener;
+        import com.google.gson.Gson;
+        import com.google.gson.reflect.TypeToken;
 
-import java.util.UUID;
+        import java.io.File;
+        import java.io.FileNotFoundException;
+        import java.io.FileReader;
+        import java.io.FileWriter;
+        import java.io.IOException;
+        import java.lang.reflect.Type;
+        import java.util.ArrayList;
+        import java.util.Date;
+        import java.util.UUID;
 
 
 /**
@@ -43,12 +59,19 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "1256";
+    private static final int NOTIFICATION_ID = 192873641;
     // Here is my first comment
     private TextView mTextMessage;
     private Toolbar myToolbar;
     private SearchView searchView;
     private TextView appName;
     private ImageView profileIcon;
+    // Firebase variables
+    private final String link = "https://ishelf-bb4e7.firebaseio.com";
+    private Firebase ref;
+    // File for saving date object for notifications
+    private static final String FILENAME = "date1.sav";
 
 
     private static final String TAG = "MainActivity";
@@ -78,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
-        };
+    };
 
     Fragment fragment;
 
@@ -86,12 +109,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //TODO remove
-        //Intent myIntent = new Intent(this, ListOfRequestsActivity.class);
-        //myIntent.putExtra("key", value); //Optional parameters
-        //this.startActivity(myIntent);
-        //TODO remove
+        // Necessary to activate notifications for the app
+        createNotificationChannel();
+        // Firebase listener, activated when there is a change to notifications
+        // Will be activated regardless of activity, or if the app is even running
+        // Set up test firebase listener
+        Firebase.setAndroidContext(this);
+        ref = new Firebase(link);
+        Firebase tempRef = ref.child("Notifications");
+        // create a one time use listener to immediately access datasnapshot
+        tempRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d(TAG + " NotifcationEvent", "Notification listener activated");
+                Log.d("snapshot values", dataSnapshot.getKey() + " " +
+                        dataSnapshot.getValue().toString());
+                String jNotification = dataSnapshot.getValue(String.class);
+                if (jNotification != null) {
+                    Log.d("jNotification", jNotification);
+                    // Get user object from Gson
+                    Gson gson = new Gson();
+                    Type tokenType = new TypeToken<Notification>() {
+                    }.getType();
+                    Notification notification = gson.fromJson(jNotification, tokenType);
+                    String username = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+                            .getString("username", "TestUsername");
+                    Date lastNotificationDate = getLastNotificationDate();
+                    setLastNotificationDate(new Date());
+                    Log.d(TAG + " notificationDate", "File notification date is set to " + lastNotificationDate.toString());
+                    // Also check the date here, to confirm it is new
+                    if (notification.getUserName().equals(username) && notification.getDate().after(lastNotificationDate)) {
+                        Log.d(TAG + " validNotification", "Firebase contains a new notification with a valid username for this user");
+                        pushNotification(notification.getText());
+                    }
+                }
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                return;
+            }
+        });
 
         SignIn();
 
@@ -122,7 +185,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 appName.setVisibility(View.INVISIBLE);
                 profileIcon.setVisibility(View.INVISIBLE);
-                
+                int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+                TextView textView = (TextView) searchView.findViewById(id);
+                textView.setTextColor(Color.CYAN);
+                textView.setHintTextColor(Color.CYAN);
             }
         });
 
@@ -149,6 +215,98 @@ public class MainActivity extends AppCompatActivity {
 
         //loadFragment(new myBooksFragment());
     }
+
+    private void pushNotification(String text) {
+        // Create the notification
+        //TODO why does it show all notifications when starting up
+        //TODO this could be fixed by deleting notifications when we see them
+        String title = "Request";
+        String content = text;
+        // Create instance of notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                //TODO notification_icon is empty right now, that may be causing errors
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        // Set context for notification
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        //TODO fix notification id, might be okay since there is only 1 notification
+        // Generate the notification
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.toolbar, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.action_favorite:
+//                // User chose the "Settings" item, show the app settings UI...
+//                Intent intent = new Intent(this, ViewProfileActivity.class);
+//                String username = getSharedPreferences("UserPreferences", Context.MODE_PRIVATE).getString("username", "TestUsername");
+//                Log.d(TAG, "onOptionsItemSelected: Username:" + username);
+//                intent.putExtra("Username", username);
+//                startActivity(intent);
+//                finish();
+//                return true;
+//
+//            case R.id.action_settings:
+//                // User chose the "Favorite" action, mark the current item
+//                // as a favorite...
+//                return true;
+//
+//            default:
+//                // If we got here, the user's action was not recognized.
+//                // Invoke the superclass to handle it.
+//                return super.onOptionsItemSelected(item);
+//
+//        }
+//    }
+
+
+
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.search_menu, menu);
+//
+//        MenuItem searchItem = menu.findItem(R.id.book_search);
+//        SearchView searchView = (SearchView) searchItem.getActionView();
+//
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onQueryTextChange(String query) {
+//        // Here is where we are going to implement the filter logic
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onQueryTextSubmit(String query) {
+//        return false;
+//    }
+
+
+
+
+
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        final MenuItem searchItem = menu.findItem(R.id.book_search);
+//        final android.support.v7.widget.SearchView searchView = (SearchView) searchItem.getActionView();
+//        searchView.setQueryHint("test for something");
+//
+//        return true;
+//    }
 
     /**
      * Check if a user is signed in, if not
@@ -226,5 +384,74 @@ public class MainActivity extends AppCompatActivity {
     public void ViewNotification(View v){
         Intent intent = new Intent(v.getContext(), NotificationActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * If the SDK version is above a certain number, we need to create the notification channel
+     * Conditional statements ensure that we are backwards compatible
+     * @author : https://developer.android.com/training/notify-user/build-notification#java
+     */
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //TODO created string value resources for channel_name and channel_description
+            // test_channel_name, test_description
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    /**
+     * Get the last time the notification listener was run
+     * This code is necessary so that not all Firebase notifications are shown when the
+     * app is completely closed and reopened. Only Notifications after this date are "new"
+     * @return
+     * @author : Randal Kimpinski
+     */
+    private Date getLastNotificationDate() {
+        try {
+            FileReader in = new FileReader(new File(getFilesDir(),FILENAME));
+            Gson gson = new Gson();
+            Type listtype = new TypeToken<ArrayList<Date>>(){}.getType();
+            ArrayList<Date> dates;
+            dates = gson.fromJson(in, listtype);
+            return dates.get(0);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
+            Date date = new Date();
+            setLastNotificationDate(date);
+            return date;
+        }
+    }
+
+    /**
+     * Set the last time the notification listener was run
+     * This code is necessary so that not all Firebase notifications are shown when the
+     * app is completely closed and reopened. Only Notifications after this date are "new"
+     * @author : Randal Kimpinski
+     */
+    private void setLastNotificationDate(Date date) {
+        try {
+            FileWriter out = new FileWriter(new File(getFilesDir(),FILENAME));
+            Gson gson = new Gson();
+            ArrayList<Date> dates = new ArrayList<>();
+            dates.add(date);
+            gson.toJson(dates, out);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
