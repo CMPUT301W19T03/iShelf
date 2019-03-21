46c4d51536330e3416424444dba6f0f7432b79ea
rkimpins <rkimpins@ualberta.ca>
2019-03-20 19:43:17 -0600

    Adding notifications, need to get the icon working properly

--- code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
+++ code/app/src/main/java/ca/ualberta/ishelf/MainActivity.java
@@ -1,9 +1,12 @@
 package ca.ualberta.ishelf;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -96,15 +99,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Necessary to activate notifications for the app
+        createNotificationChannel();
+        // Create instance of notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 //TODO notification_icon is empty right now, that may be causing errors
-                .setSmallIcon(R.drawable.notification_icon)
+                .setSmallIcon(R.drawable.  notification_icon)
                 .setContentTitle("testTitle")
                 .setContentText("testContent")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        // Set context for notification
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         // notificationId is a unique int for each notification that you must define
         //TODO fix notification id, might be okay since there is only 1 notification
+        // Generate the notification
         notificationManager.notify(NOTIFICATION_ID, builder.build());
 
 
@@ -137,12 +145,6 @@ public class MainActivity extends AppCompatActivity {
                         //TODO this could be fixed by deleting notifications when we see them
                     }
                 }
-                /*
-                if (previousChildName != null) {
-                    String test = dataSnapshot.child(previousChildName).getValue().toString();
-                    Log.d("test", test);
-                }
-                */
                 //String jUser = dataSnapshot.getValue(String.class);
                 // Write code to loop through notifications here
             }
@@ -367,5 +369,22 @@ public class MainActivity extends AppCompatActivity {
     public void ViewNotification(View v){
         Intent intent = new Intent(v.getContext(), NotificationActivity.class);
         startActivity(intent);
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //TODO created string value resources for channel_name and channel_description
+            // test_channel_name, test_description
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
